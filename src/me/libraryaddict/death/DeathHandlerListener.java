b95dc204240de0f300203b8d353898fe0ae1abd6
libraryaddict <libraryaddict115@yahoo.co.nz>
2014-03-21 22:14:20 +1300

    Handle better for when damages should be cleared. Add support for pushed fall deathcause

--- src/me/libraryaddict/death/DeathHandlerListener.java
+++ src/me/libraryaddict/death/DeathHandlerListener.java
@@ -5,25 +5,50 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 import me.libraryaddict.damageapi.DamageApi;
+import me.libraryaddict.death.causes.DeathCauseFight;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class DeathHandlerListener implements Listener {
     private HashMap<Player, ArrayList<Damage>> damageCauses = new HashMap<Player, ArrayList<Damage>>();
+    private HashMap<Player, Damage> pushed = new HashMap<Player, Damage>();
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event) {
+        damageCauses.remove(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerDeathEvent event) {
+        damageCauses.remove(event.getEntity());
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        if (pushed.containsKey(event.getPlayer())) {
+            if (event.getPlayer().getFallDistance() == 0) {
+                pushed.remove(event.getPlayer());
+            }
+        }
+    }
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onDamage(EntityDamageEvent event) {
         if (event.getEntity() instanceof Player && !event.isCancelled()) {
             DeathCause cause = DeathCause.getDeathCause(event);
             if (cause != DeathCause.UNKNOWN || event.getDamage() != 0) {
-                if (damageCauses.containsKey(event.getEntity())) {
-                    double hp = ((Player) event.getEntity()).getMaxHealth() - ((Player) event.getEntity()).getHealth();
-                    Iterator<Damage> itel = damageCauses.get(event.getEntity()).iterator();
+                Player p = (Player) event.getEntity();
+                if (damageCauses.containsKey(p)) {
+                    double hp = p.getMaxHealth() - p.getHealth();
+                    Iterator<Damage> itel = damageCauses.get(p).iterator();
                     while (itel.hasNext()) {
                         Damage dmg = itel.next();
                         if (hp <= 0) {
@@ -33,11 +58,24 @@ public class DeathHandlerListener implements Listener {
                         }
                     }
                 } else {
-                    damageCauses.put((Player) event.getEntity(), new ArrayList<Damage>());
+                    damageCauses.put(p, new ArrayList<Damage>());
                 }
-                double damage = DamageApi
-                        .calculateDamageAddArmor((Player) event.getEntity(), event.getCause(), event.getDamage());
-                damageCauses.get(event.getEntity()).add(0, new Damage(cause, damage, cause.getKiller(event)));
+                double damage = DamageApi.calculateDamageAddArmor(p, event.getCause(), event.getDamage());
+                Damage newDamage = new Damage(cause, damage, cause.getKiller(event));
+                damageCauses.get(p).add(0, newDamage);
+                if (pushed.containsKey(p)) {
+                    if (newDamage.getCause() == DeathCause.FALL) {
+                        newDamage.setCause(DeathCause.PUSHED_FALL);
+                        newDamage.setDamager(pushed.remove(p).getDamager());
+                    } else if (p.getFallDistance() == 0) {
+                        pushed.remove(p);
+                    }
+                }
+                if (!p.getAllowFlight() && p.getFallDistance() == 0) {
+                    if (newDamage.getCause() instanceof DeathCauseFight || newDamage.getCause() == DeathCause.SHOT) {
+                        pushed.put(p, newDamage);
+                    }
+                }
             }
         }
     }
